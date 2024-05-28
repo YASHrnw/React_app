21d49173d29d0e5363cf9465efde25c3acf38df3
YASHrnw <f347yash@gmail.com>
2024-05-28 12:57:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import ObjForm2 from './ObjForm2'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <ObjForm2/>
+
+    </>
+  )
+}
+
+export default App
