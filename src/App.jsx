1f0f538a25c8072fbf8ce285849149af4521c16b
YASHrnw <f347yash@gmail.com>
2024-06-04 12:55:53 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
 import './App.css'
-import ObjForm2 from './ObjForm2'
+import Form from './Componets/Form'
+// import ObjForm2 from './ObjForm2'
+
+
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <ObjForm2/>
+      {/* <ObjForm2/> */}
+      <Form/>
+ 
+
+
+
 
     </>
   )
---
+++ src/Componets/ObjForm2.jsx
@@ -0,0 +1,107 @@
+
+// import React, { useState } from 'react'
+
+// export default function ObjForm2() {
+
+//   const [obj, setobj] = useState({
+//     name: "",
+//     username: "",
+//     password: "",
+//     gender: "",
+//   })
+
+
+//   const handlechange = (e) => {
+//     const { name, value } = e.target;
+//     setobj({
+//       ...obj,
+//       [name]: value
+//     });
+//   };
+
+
+//   const handleform = (e) => {
+//     e.preventDefault()
+//     console.log(obj);
+//     console.log(obj.name);
+//     console.log(obj.username);
+//     console.log(obj.password);
+//     console.log(obj.gender);
+//     setobj({
+//       name: "",
+//       username: "",
+//       password: "",
+//       gender: "",
+//     })
+//   }
+
+//   return (
+//     <>
+//       <form onSubmit={handleform}>
+//         <input type="text" placeholder='Enter your Name' name="name" value={obj.name} onChange={handlechange} />
+//         <input type="text" placeholder="Enter Username" name="username" value={obj.username} onChange={handlechange} />
+//         <br />
+//         <input type="password" placeholder="Enter your password" name="password" value={obj.password} onChange={handlechange} />
+
+//         <br />
+//         <input type="radio" name="gender" id="male" value="male" onChange={handlechange} />
+//         <label htmlFor="male">Male</label>
+
+//         <input type="radio" name="gender" id="female" value="female" onChange={handlechange} />
+//         <label htmlFor="female">Female</label>      <br />
+
+
+
+//         <button>Submit</button>
+//       </form>
+
+//     </>
+//   )
+// }
+
+
+
+
+
+
+// export default function ObjForm2() {
+
+//   const [obj1, setobj] = useState({
+//     name: "",
+//     Email: "",
+
+//   })
+
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setobj({
+//       ...obj1,
+//       [name]: value
+//     });
+//   };
+
+//   const handleform = (e) => {
+//     e.preventDefault()
+//     console.log(obj1);
+//     console.log(obj1.name);
+//     console.log(obj1.Email);
+//     setobj({
+//       name: "",
+//       Email: "",
+//     })
+//   }
+
+//   return (
+//     <><form onSubmit={handleform}>
+//       <input type="text" name="name" value={obj1.name} onChange={handleChange} />
+//       <input type="text" name="Email" value={obj1.Email} onChange={handleChange} />
+//       <button>submit</button>
+//     </form>
+
+//     </>
+//   )
+// }
+
+
+  
