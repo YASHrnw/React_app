2b2c65dd65cf10d125d8d4c60b2f404b56be5e5a
YASHrnw <f347yash@gmail.com>
2024-06-13 13:31:23 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
 import './App.css';
 
 //Counterpro
-// import Content from './Projects/Counterpro/Content';
-// import Footer from './Projects/Counterpro/Footer';
 // import Header from './Projects/Counterpro/Header';
+// import Footer from './Projects/Counterpro/Footer';
 // import Sidebar from './Projects/Counterpro/SideBar';
+// import Content from './Projects/Counterpro/Content';
 import "bootstrap/dist/css/bootstrap.min.css"
-import OnChangeTextFun from "./Projects/Array/OnChangeTextClass"
-import Onchange from './Projects/Array/Onchange';
+
 
 // Array
 // import Es6Array from './Projects/Array/Es6Array'
 // import CalcClass from './Projects/Array/CalcClass'
 // import Counter from './Project/Array/Counter'
+// import OnChangeTextFun from "./Projects/Array/OnChangeTextClass"
+// import Onchange from './Projects/Array/Onchange';
+// import Array from './Projects/Array'
 // import Event from './Projects/event/Event'
+
+
+
+
 // import PropDriling from './Componets/PropDriling'
 // import Form from './Componets/Form'
-// import Array from './Projects/Array'
 // import ObjForm2 from '.Componets/ObjForm2'
+import LocalCrud from './Componets/LocalCrud'
 
 
 
@@ -27,31 +33,36 @@ import Onchange from './Projects/Array/Onchange';
 function App() {
   return (
     <>
-
-    <OnChangeTextFun/>
-    <Onchange/>
-      {/* //Asssinment? */}
-      {/* <Es6Array />
-      <CalcClass />
-      <Counter /> */}
-
-
       {/* <Header />
       <Sidebar />
       <Footer />
       <Content /> */}
 
+      {/* //AsssinmentAll */}
+      {/* <Es6Array /> */}
+      {/* <CalcClass /> */}
+      {/* <Counter /> */}
+      {/* <OnChangeTextFun/> */}
+      {/* <Onchange/> */}
       {/* <Event/> */}
 
 
 
 
+
+
+
+      {/* *************************************************ComponentsAll************************/}
       {/* <ObjForm2/> */}
       {/* <Form/> */}
       {/* <Array /> */}
       {/* <PropDriling/> */}
+      <LocalCrud />
 
 
+
+
+      {/* ****************************************************Prectice*************************** */}
 
 
     </>
---
+++ src/Componets/LocalCrud.jsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react"
+
+
+export default function LocalCrud() {
+
+    const [iteam, setitem] = useState(() => {
+        const storeData = localStorage.getItem("Data");
+        return storeData ? JSON.parse(storeData) : [];
+
+    })
+
+
+    const [inputvalue, setInputvalue] = useState({
+        name: "",
+        password: ""
+    });
+
+    const handleinput = (e) => {
+        const { name, value } = e.target;
+        setInputvalue({ ...inputvalue, [name]: value })
+
+    }
+
+    const handlAddData = (e) => {
+        e.preventDefault();
+
+        if (inputvalue.name === "") {
+            alert("please enter a name")
+        } else if (inputvalue.name.length < 2) {
+            alert("please Enter more then 2 characters")
+        } else if (inputvalue.password === "") {
+            alert("please enter a password")
+        } else if (inputvalue.password.length < 8) {
+            alert("please Enter the password More then 4 character")
+        } else {
+            setitem([...iteam, inputvalue])
+            console.log(inputvalue);
+        }
+    }
+
+    useEffect(() => {
+        localStorage.setItem("Data", JSON.stringify(iteam))
+    }, [iteam])
+
+    return (
+        <>
+            <form action="">
+
+                <input type="text" placeholder="Enter Name" name="name" value={inputvalue.name} onChange={handleinput} /><br /><br />
+                <input type="password" placeholder="Enter password" name="password" value={inputvalue.password} onChange={handleinput} /><br /><br />
+
+                <button onClick={handlAddData}>Add Data</button>
+            </form>
+
+            {
+                <table border={2}  cellSpacing={10} >
+                    <thead >
+                        <tr>
+                            <th>SR NO</th>
+                            <th>Name</th>
+                            <th>Password</th>
+                           
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {iteam.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.password}</td>
+                                </tr>
+                            );
+                        })}
+
+                    </tbody>
+
+
+                </table>
+
+
+
+
+
+
+
+            }
+        </>
+    )
+}
